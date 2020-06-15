541f2fe35ab7a45861af10882eb3401ccdf88ac8
thomas.souksouvanh@sfr.fr <thomas.souksouvanh@sfr.fr>
2020-06-15 09:27:27 +0200

    first commit

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])->add('lastname', TextType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Prenom'
+                ]
+            ])
+            ->add('email', EmailType::class,[
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Accepter termes et conditions',
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Vous devez accepter les termes et conditions',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class,[
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'required' => true,
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Mot de passe'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => false,
+                    'attr' => [
+                        'placeholder' => 'Confirmation du mot de passe'
+                    ]
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'S\'il vous plait veuillez rentrer un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} charactères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 40,
+                    ]),
+                ],
+            ])
+        ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
---
+++ src/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('slug')
+            ->add('reset_token')
+            ->add('activation_token')
+            ->add('avatar')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
